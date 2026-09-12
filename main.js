4b954d626f38bb7a949c29de5cda1680f29130b1
Vinicius Pereira Raposo <66894455+FishRaposo@users.noreply.github.com>
2026-09-12 18:58:09 -0300

    Switch contact email to fishraposo@gmail.com

--- main.js
+++ main.js
@@ -75,7 +75,7 @@
     '      <a href="https://www.upwork.com/freelancers/~01ca94c53d0d0101f3" target="_blank" rel="noopener">Upwork</a>',
     '      <a href="https://github.com/FishRaposo" target="_blank" rel="noopener">GitHub</a>',
     '      <a href="https://x.com/FishRaposo" target="_blank" rel="noopener">X</a>',
-    '      <a href="mailto:viniciusraposo71@hotmail.com">Email</a>',
+    '      <a href="mailto:fishraposo@gmail.com">Email</a>',
     '    </nav>',
     '  </div>',
     '  <p class="footer-copyright">&copy; 2026 Vinícius Raposo. All rights reserved.</p>',
